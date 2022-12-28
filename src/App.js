b1f2e1bf8db95da9274766f6752b43f03501bbd9
RanaMohammadi <itsmerana.m@gmail.com>
2022-12-28 15:55:52 +0100

    added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from "./logo.jpeg";
+import logo from "./book.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <div className="logo-img">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            Coded by{" "}
+            <a href="https://github.com/RanaMohammadi/dictionary-project">
+              Rana Mohammadi
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={handleKeyword} />
+      </form>
+    </div>
+  );
+}
